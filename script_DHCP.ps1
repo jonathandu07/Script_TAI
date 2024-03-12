320a41e7b16bc8e5721d0fa9679ef59fa36a6389
jonathan YHWH <93312510+jonathandu07@users.noreply.github.com>
2024-03-12 10:54:59 +0100

    Script avant le redémarrage

--- script_DHCP.ps1
+++ script_DHCP.ps1
@@ -1,3 +1,13 @@
 # Mettre à jour le système et installer le rôle DHCP
 Write-Output "Mise à jour du système et installation du rôle DHCP..."
 $installResult = Install-WindowsFeature -Name DHCP -IncludeManagementTools
+
+# Vérifier si le redémarrage est nécessaire
+if ($installResult.RestartNeeded -eq "Oui") {
+    Write-Output "Un redémarrage est nécessaire pour terminer l'installation. Le serveur va redémarrer..."
+    # Renommer le serveur en SRV-DHCP avant de redémarrer
+    Rename-Computer -NewName "SRV-DHCP" -Force
+    # Le script s'arrête ici, et vous devez redémarrer manuellement le serveur.
+    Restart-Computer
+    exit
+}
